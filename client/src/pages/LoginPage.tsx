8a16b509ce71b90486d71a56645e94074297674f
Juan Carlos Bautista <jcbautista2701@gmail.com>
2025-05-29 00:07:09 -0500

    login completed

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -4,14 +4,24 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../components/ui/carousel";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Separator } from "../components/ui/separator";
 import { useAuthStore } from "../store/auth";
 import { useNavigate } from "react-router-dom";
+import b1 from "../assets/b1.svg";
+import b2 from "../assets/b2.svg";
 
 export function LoginPage() {
   const setToken = useAuthStore((state) => state.setToken);
@@ -44,9 +54,33 @@ export function LoginPage() {
   }
   return (
     <main className="h-screen flex w-full">
-      <div className="bg-primary-foreground w-full h-full flex p-16"></div>
+      <div className="hidden md:flex bg-primary-foreground w-full h-full p-16 items-center justify-center">
+        <Carousel
+          className="w-full max-w-xl"
+          autoplay={5500}
+          // plugins={[Autoplay({ delay: 1000 })]}
+          opts={{
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            <CarouselItem>
+              <div className="flex aspect-square rounded p-8">
+                <img src={b1} alt="business1" />
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="flex aspect-square rounded p-8">
+                <img src={b2} alt="business2" />
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
       <section className="flex items-center justify-center bg-background h-full max-w-3xl w-full p-4">
-        <Card className="w-[350px]">
+        <Card className="w-full max-w-md">
           <CardHeader>
             <CardTitle className="text-2xl font-bold tracking-tighter">
               Entrar con su contraseña
@@ -79,14 +113,22 @@ export function LoginPage() {
 
               <Button className="mt-6 w-full">Entrar</Button>
 
-              <div className="flex items-center gap-6">
+              <div className="flex items-center gap-6 mt-4">
                 <Separator />
                 <span className="text-xs text-muted-foreground">or</span>
                 <Separator />
               </div>
-              <Button variant={"outline"} className="mt-6 w-full">Registrar</Button>
+              <Button variant={"outline"} className="mt-4 w-full">
+                Registrar
+              </Button>
             </form>
           </CardContent>
+          <CardFooter>
+            <p className="text-muted-foreground text-center text-sm">
+              Al entrar en la plataforma usted esta de acuerdo con nuestros
+              terminos de uso y politica de privacidad{" "}
+            </p>
+          </CardFooter>
         </Card>
       </section>
     </main>
